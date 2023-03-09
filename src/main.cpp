f714c4c0692af5cca4faa2c9a2a4976ca9e2c83b
bakhtiarZ <93926720+bakhtiarZ@users.noreply.github.com>
2023-03-09 14:06:53 +0000

    Made it more thread safe

--- src/main.cpp
+++ src/main.cpp
@@ -158,7 +158,7 @@ void scanKeysTask(void * pvParameters) {
       keyArray[i] = readCols();
     }
     uint8_t keyArrayCopy[7]; 
-    memcpy(&keyArrayCopy, (void*)&keyArray, sizeof keyArray);
+    memcpy(keyArrayCopy,(void*)keyArray, sizeof keyArray);
     xSemaphoreGive(keyArrayMutex);
       uint8_t keyArraycopyy[7];
       uint16_t keys_pressed_copy = 0;
@@ -254,22 +254,21 @@ void CANSendTask(void * pvParameters){
 void CANDecodeTask(void * pvParameters){ 
   uint32_t localCurrentStepSize = 0;
   uint8_t localRX_Message[8];
-  xSemaphoreTake(rxmsgMutex, portMAX_DELAY);
-  memcpy(&localRX_Message, (void*)&RX_Message, sizeof RX_Message);
-  xSemaphoreGive(rxmsgMutex);
   while(1){
-    xQueueReceive(msgInQ, RX_Message, portMAX_DELAY);
-    if(RX_Message[0] == 0x52){
+    xQueueReceive(msgInQ, localRX_Message, portMAX_DELAY);
+    if(localRX_Message[0] == 0x52){
       __atomic_store_n(&currentStepSize, 0, __ATOMIC_RELAXED);
     }
-    else if(RX_Message[0] == 0x50){
-      if(RX_Message[1] >4)
-      localCurrentStepSize = stepSizes[RX_Message[2]] << (RX_Message[1] - 4);
+    else if(localRX_Message[0] == 0x50){
+      if(localRX_Message[1] >4)
+      localCurrentStepSize = stepSizes[localRX_Message[2]] << (localRX_Message[1] - 4);
       else
-      localCurrentStepSize = stepSizes[RX_Message[2]] >> (4 - RX_Message[1]);
+      localCurrentStepSize = stepSizes[localRX_Message[2]] >> (4 - localRX_Message[1]);
       __atomic_store_n(&currentStepSize, localCurrentStepSize, __ATOMIC_RELAXED);
     }
-   
+    xSemaphoreTake(rxmsgMutex, portMAX_DELAY);
+    memcpy(RX_Message, localRX_Message, sizeof RX_Message);
+    xSemaphoreGive(rxmsgMutex);
   }
 }
 
